35e279d4cfc1c07fdf119f2d8673b11fa9e4b0fd
nguyennhulonggithub <80632602+nguyennhulonggithub@users.noreply.github.com>
2022-06-02 19:56:02 +0700

    long update

--- src/main/java/com/server/AdditionalResourceWebConfiguration.java
+++ src/main/java/com/server/AdditionalResourceWebConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AdditionalResourceWebConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        loi o da
+
         registry.addResourceHandler("/image/**")
                 .addResourceLocations("file:src\\main\\java\\com\\server\\assets\\");
     }
