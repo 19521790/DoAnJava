c9e57fa7eb471f4d81109ccea73c4d863407d04b
nguyennhulonggithub <80632602+nguyennhulonggithub@users.noreply.github.com>
2022-10-05 18:29:34 +0700

    new

--- src/main/java/com/doan/client/Application.java
+++ src/main/java/com/doan/client/Application.java
@@ -38,6 +38,40 @@ public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException, UnirestException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/com/doan/client/View/UserScreen/MainScreen.fxml"));
+        // FXMLLoader fxmlLoader= new
+        // FXMLLoader(getClass().getResource("/com/doan/client/View/AdminScreen/AdminScreen.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/com/doan/client/application.css").toExternalForm());
+        primaryStage.getIcons()
+                .add(new Image(getClass().getResource("/com/doan/client/Image/icon.jpg").toExternalForm()));
+        primaryStage.setTitle("Zyan");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        PublicController.primaryStage = primaryStage;
+        PublicController.userScene = scene;
+        Unirest.setObjectMapper(new ObjectMapper() {
+            com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+
+            public String writeValue(Object value) {
+                try {
+                    return mapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    return mapper.readValue(value, valueType);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
 
     }
 
